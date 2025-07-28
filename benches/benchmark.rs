e4b525c21085cff27bfec230a383b26da0067d24
tomara_x <414d59@gmail.com>
2025-07-28 03:36:03 +0300

    update dependencies

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,5 +1,6 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 use fundsp::hacker32::*;
+use std::hint::black_box;
 
 fn sine_bench(_dummy: usize) -> Wave {
     Wave::render(
